9dbb77e7aa9b3be539780d765035f68df69244a7
Jason-Cooke <jason.b.cooke@gmail.com>
2019-03-19 10:14:45 +1300

    Fix function typo
    Should read getOrganisation, as getOragnization is declared below

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-exports.getOrganization = function(orgName) {
+exports.getOrganisation = function(orgName) {
   fetch(`https://api.github.com/orgs/${orgName}`)
     .then(res => res.json())
     .then(json => console.log(json))
