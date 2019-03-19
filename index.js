fac2a4b3a18904b9b02669a371bc8b4328dc4485
Jason-Cooke <jason.b.cooke@gmail.com>
2019-03-19 13:12:18 +1300

    Return result rather than logging it

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
 
 exports.getOrganisation = function(orgName) {
-  fetch(`https://api.github.com/orgs/${orgName}`)
+  return fetch(`https://api.github.com/orgs/${orgName}`)
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => json)
+    .catch(e => {error: e});
 }
 
 exports.getOrganization = exports.getOrganisation;
