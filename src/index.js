c64811c55d445aeb476039dff23be7ecb6c5bde4
Jason-Cooke <jason.b.cooke@gmail.com>
2019-03-26 14:44:54 +1300

    Move index.js to src/index.js
    Source goes here, and build will output to lib/index.js.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+exports.getOrganisation = function(orgName) {
+  return fetch(`https://api.github.com/orgs/${orgName}`)
+    .then(res => res.json())
+    .then(json => json)
+    .catch(e => {error: e});
+}
+
+exports.getOrganization = exports.getOrganisation;
+
+exports.getPublicUsersFromOrg = async function(orgName, cb) {
+  const org = await exports.getOrganisation(orgName);
+  try {
+    const publicMembersUrl = org.public_members_url.replace(/{.*}/g, '');
+    return fetch(publicMembersUrl)
+      .then(res => res.json())
+      .then(json => cb(null, json));
+  } catch(error) {
+    return cb(error, null);
+  }
+}
