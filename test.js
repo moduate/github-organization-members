fe398537caf29466ba709459aceffe2f8cef5a78
Jason-Cooke <jason.b.cooke@gmail.com>
2019-04-01 14:33:30 +1300

    Add test case and use constant for org name

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const gom = require('./src');
 
 const BASE_URL = "https://api.github.com";
+const ORGANIZATION_NAME = "Moduate";
 
 describe('Calling getOrganization', () => {
   test('passing an empty string as argument, fails with "Not Found"', async () => {
@@ -17,14 +18,27 @@ describe('Calling getOrganization', () => {
     expect(data.message).toBe("Not Found");
   });
   test('passing "Moduate" as an argument returns the Moduate organization object', async () => {
-    const data = await gom.getOrganization("Moduate");
-    expect(data.name).toBe("Moduate");
+    const data = await gom.getOrganization(ORGANIZATION_NAME);
+    expect(data.name).toBe(ORGANIZATION_NAME);
   });
 })
 
 describe('Calling getOrganisation', () => {
   test('passing "Moduate" as an argument returns the Moduate organization object', async () => {
-    const data = await gom.getOrganisation("Moduate");
-    expect(data.name).toBe("Moduate");
+    const data = await gom.getOrganisation(ORGANIZATION_NAME);
+    expect(data.name).toBe(ORGANIZATION_NAME);
+  });
+})
+
+describe('Get Members', () => {
+  test('Passing Moduate as an argument, returns at least one public member', async () => {
+    const data = await gom.getPublicUsersFromOrg(ORGANIZATION_NAME);
+    gom.getPublicUsersFromOrg(orgName, (error, members) => {
+      if(error) {
+        console.log(error);
+      }
+      console.log(members);
+    });
+    expect(data).toHaveLength()
   });
 })
