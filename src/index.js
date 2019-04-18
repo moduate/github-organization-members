2cf4f2b27df2357e8e7537ef98cba878b9b3ed68
Jason-Cooke <jason.b.cooke@gmail.com>
2019-04-18 13:58:17 +1200

    Move fetch outside of try/catch block

--- src/index.js
+++ src/index.js
@@ -14,15 +14,19 @@ exports.getOrganization = exports.getOrganisation;
 
 exports.getPublicUsersFromOrg = async (orgName, cb) => {
   const org = await exports.getOrganisation(orgName);
+  // if not org.id, then an invalid org name was passed into getOrganisation
+  // and we should return early
   if(!org.id) {
     return org;
   }
+  let publicMembersUrl;
   try {
-    const publicMembersUrl = org.public_members_url.replace(/{.*}/g, '');
-    return fetch(publicMembersUrl)
-      .then(res => res.json())
-      .then(json => cb(null, json));
+    publicMembersUrl = org.public_members_url.replace(/{.*}/g, '');
   } catch(error) {
     return cb(error, null);
   }
+  return fetch(publicMembersUrl)
+    .then(res => res.json())
+    .then(json => cb(null, json))
+    .catch(e => cb(error, null));
 }
